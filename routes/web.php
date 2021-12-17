6c805efe2610f742173f08236b3c21080af69b45
tql054 <tuanquocle2001@gmail.com>
2021-12-17 22:34:47 +0700

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -34,10 +34,14 @@ Route::get('/add_category', 'CategoryProduct@show_form_add_category');
 Route::get('/all_category', 'CategoryProduct@show_all_category');
 Route::post('/save_category', 'CategoryProduct@add_category');
 Route::get('/delete_category/{categoryid}', 'CategoryProduct@delete_category');
+Route::get('/update_category/{categoryid}', 'CategoryProduct@show_form_edit_category');
+Route::post('/save_editcategory', 'CategoryProduct@update_category');
 
 // admin nxb
 Route::get('/all_nxb', 'NXBController@show_all_NXB');
 Route::get('/add_nxb', 'NXBController@show_form_add_NXB');
 Route::get('/delete_nxb/{nxbid}', 'NXBController@delete_nxb');
 Route::post('/save_nxb', 'NXBController@add_nxb');
+Route::get('/update_nxb/{nxbid}', 'NXBController@show_form_edit_NXB');
+Route::post('/save_editnxb', 'NXBController@update_NXB');
 // END ADMIN
